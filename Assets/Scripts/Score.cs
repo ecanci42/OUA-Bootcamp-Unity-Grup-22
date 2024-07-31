bd14910f8b09c911b2a7eb482ac2b43a59f2f0a7
Emirhan Turan <emirhanturanfb@hotmail.com>
2024-07-31 13:31:25 +0300

    delete deneme

--- Assets/Scripts/Score.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Score : MonoBehaviour
-{
-    public static int totalscore;
-    public static int live=3;
-}
